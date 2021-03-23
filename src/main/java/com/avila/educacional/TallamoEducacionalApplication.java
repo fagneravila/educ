6fbcb87e0232d8ce27458ba919d355bf769a8953
Fagner Avila <fagner_avila99@hotmail.com>
2021-03-22 23:20:03 -0300

    criacao de projeto

---
+++ src/main/java/com/avila/educacional/TallamoEducacionalApplication.java
@@ -0,0 +1,13 @@
+package com.avila.educacional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallamoEducacionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallamoEducacionalApplication.class, args);
+	}
+
+}
